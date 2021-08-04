c0ae9dffb5ad4d0fc4c955cab4a81ee059539ee0
David Stoneking <76399396+drs021@users.noreply.github.com>
2021-08-04 15:27:37 -0500

    Add files via upload

---
+++ Is it a prime number.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 20 10:04:38 2021
+
+@author: David Stoneking
+"""
+
+# Define a function that takes one integer argument and returns logical value true or false depending on if the integer is a prime.
+
+# Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+# Requirements
+# You can assume you will be given an integer input.
+# You can not assume that the integer will be only positive. You may be given negative numbers as well (or 0).
+# NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. Numbers go up to 2^31 (or similar, depends on language version). Looping all the way up to n, or n/2, will be too slow.
+# Example
+# is_prime(1)  /* false */
+# is_prime(2)  /* true  */
+# is_prime(-1) /* false */
+
+
+def is_prime(num):
+    divide_by = range(2,100000)
+    if num <= 0:
+        print('False')
+        return False
+    else:
+        pass
+    for ea in divide_by:
+        if num % ea == 0:
+            print('False')
+            return False
+    print('True')
+    return True
+
+is_prime(0)
+
+is_prime(5099)
